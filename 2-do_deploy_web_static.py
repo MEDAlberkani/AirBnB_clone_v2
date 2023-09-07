8c8cfe9478204dd6b4d7f767807609283bfc6b33
Mohammed <jamalbero18@gmail.com>
2023-09-07 02:42:36 -0700

    Task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,63 +1,52 @@
 #!/usr/bin/python3
-
 """
-This Fabric script deploys web static content to remote servers.
+Fabric script that distributes an archive to your web servers
 """
 
-from fabric.api import env, put, run
-from os.path import exists
+from datetime import datetime
+from fabric.api import *
 import os
 
-# Define the remote hosts
-env.hosts = ['18.207.234.171', '35.153.226.243']
+env.hosts = ["44.210.150.159", "35.173.47.15"]
+env.user = "ubuntu"
+
+
+def do_pack():
+    """
+        return the archive path if archive has generated correctly.
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
 
 
 def do_deploy(archive_path):
     """
-    This function deploys an archive to the web servers.
-
-    :param archive_path: The path to the archive to be deployed.
-    :type archive_path: str
-    :return: True if successful, False otherwise.
-    :rtype: bool
+        Distribute archive.
     """
-    # Check if archive_path exists
-    if not exists(archive_path):
-        return False
-
-    # Get the file name without extension
-    archive_name = os.path.basename(archive_path)
-    archive_name_no_ext = os.path.splitext(archive_name)[0]
-
-    try:
-        # Upload archive to the temporary folder on the web server
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
-
-        # Create the directory where the code will be deployed
-        run("sudo mkdir -p /data/web_static/releases/{}/"
-            .format(archive_name_no_ext))
-
-        # Uncompress the archive into the deployment folder
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_name, archive_name_no_ext))
-
-        # Remove the archive from the server
-        run("sudo rm /tmp/{}".format(archive_name))
-
-        # Move the files to a new folder and delete the old symbolic link
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/"
-            .format(archive_name_no_ext, archive_name_no_ext))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(archive_name_no_ext))
-
-        # Delete the old symbolic link and create a new one
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ \
-                /data/web_static/current".format(archive_name_no_ext))
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
+        print("New version deployed!")
         return True
 
-    except Exception as e:
-        # If there is an error, return False
-        return False
+    return False
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,103 +1,51 @@
 #!/usr/bin/python3
-"""This script is used to create an archive file and deploy it to a
-remote server using Fabric.
-"""
-
-# Import required modules.
-from fabric.api import local
-from datetime import datetime
-from fabric.api import run, env, put
+"""Create and distributes an archive to web servers"""
 import os.path
+import time
+from fabric.api import local
+from fabric.operations import env, put, run
 
-# Define the remote server details.
-env.hosts = ['18.207.234.171', '35.153.226.243']
-env.key_filename = '~/.ssh/school'
-env.user = 'ubuntu'
+env.hosts = ['44.210.150.159', '35.173.47.15']
 
 
 def do_pack():
-    """
-    Compress a file and return its path.
-    """
+    """Generate an tgz archive from web_static folder"""
     try:
-        # Save the current timestamp and create a filename.
-        time_now = datetime.now().strftime("%Y%m%d%H%M%S")
-        file_path = "versions/web_static_{}.tgz".format(time_now)
-
-        # Create a directory called versions.
         local("mkdir -p versions")
-
-        # Create an archive file.
-        local("tar -cvzf {} web_static/".format(file_path))
-
-        # Return the path to the archive file created.
-        return "{}".format(file_path)
-
-    except Exception as e:
-        # Return None if an error occurs.
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(time.
+                                                    strftime("%Y%m%d%H%M%S")))
+    except:
         return None
 
 
 def do_deploy(archive_path):
-    """
-    Deploy code and decompress it.
-    """
-    if not os.path.isfile(archive_path):
-        # Return False if the file does not exist.
+    """Distribute an archive to web servers"""
+    if (os.path.isfile(archive_path) is False):
         return False
 
-    # Get the filename and extension of the compressed file.
-    compressed_file = archive_path.split("/")[-1]
-    no_extension = compressed_file.split(".")[0]
-
     try:
-        # Define the remote path and symbolic link.
-        remote_path = "/data/web_static/releases/{}/".format(no_extension)
-        sym_link = "/data/web_static/current"
-
-        # Copy the archive to the remote server.
+        file = archive_path.split("/")[-1]
+        folder = ("/data/web_static/releases/" + file.split(".")[0])
         put(archive_path, "/tmp/")
-
-        # Create the remote directory.
-        run("sudo mkdir -p {}".format(remote_path))
-
-        # Extract the contents of the archive to the remote directory.
-        run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file,
-                                                  remote_path))
-
-        # Remove the archive file from the remote server.
-        run("sudo rm /tmp/{}".format(compressed_file))
-
-        # Move the contents of the web_static directory to remote directory.
-        run("sudo mv {}/web_static/* {}".format(remote_path, remote_path))
-
-        # Remove the web_static directory from the remote directory.
-        run("sudo rm -rf {}/web_static".format(remote_path))
-
-        # Remove the symbolic link to the current version.
-        run("sudo rm -rf /data/web_static/current")
-
-        # Create a new symbolic link to the current version.
-        run("sudo ln -sf {} {}".format(remote_path, sym_link))
-
-        # Return True if successful.
+        run("mkdir -p {}".format(folder))
+        run("tar -xzf /tmp/{} -C {}".format(file, folder))
+        run("rm /tmp/{}".format(file))
+        run("mv {}/web_static/* {}/".format(folder, folder))
+        run("rm -rf {}/web_static".format(folder))
+        run('rm -rf /data/web_static/current')
+        run("ln -s {} /data/web_static/current".format(folder))
+        print("Deployment done")
         return True
-
-    except Exception as e:
-        # Return False if an error occurs.
+    except:
         return False
 
 
 def deploy():
-    """
-    Create and deploy an archive to a web server.
-    """
-    # Create the archive.
-    file_path = do_pack()
-
-    if file_path is None:
-        # Return False if the archive creation fails.
+    """Create and distributes an archive to web servers"""
+    try:
+        path = do_pack()
+        return do_deploy(path)
+    except:
         return False
-
-    # Deploy the archive to the remote server.
-    return do_deploy(file_path)
